c124953c9c8134a3e0ca4d6f46ad68340ea6c34d
ayuarista <aristaadewi8@gmail.com>
2025-01-28 14:26:22 +0800

    add popular place

--- src/script.js
+++ src/script.js
@@ -8,3 +8,37 @@ function categoryItem() {
       ]
   };
 }
+function popularPlace(){
+    return{
+        items:[
+            {
+                id: 1,
+                discount: "20%",
+                image: "https://plus.unsplash.com/premium_photo-1707944422423-314b12c3885a?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                title: "Indonesia",
+                location: "Bali, Nusa Penida",
+            },
+            {
+                id: 2,
+                discount: "20%",
+                image: "https://images.unsplash.com/photo-1584872589930-e99fe5bf4408?q=80&w=877&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                title: "China",
+                location: "China, Beijing",
+            },
+            {
+                id: 3,
+                discount: "20%",
+                image: "https://images.unsplash.com/photo-1542051841857-5f90071e7989?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                title: "Japan",
+                location: "Japan, Tokyo",
+            },
+            {
+                id: 4,
+                discount: "20%",
+                image: "https://images.unsplash.com/photo-1472148439583-1f4cf81b80e0?q=80&w=1031&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                title: "Singapore",
+                location: "Singapore, Paris",
+            },
+        ]
+    }
+}
