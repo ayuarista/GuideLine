30e6217a3a707b1d71fa023dee1be35f86c5d8ca
ayuarista <aristaadewi8@gmail.com>
2025-01-28 13:32:58 +0800

    create function category

---
+++ src/script.js
@@ -0,0 +1,30 @@
+function category() {
+  return {
+    items: [
+      {
+        id: 1,
+        icons: <ion-icon name="subway-outline"></ion-icon>,
+        name: "Beach",
+        rating: "4.9",
+      },
+      {
+        id: 2,
+        icons: <ion-icon name="restaurant-outline"></ion-icon>,
+        name: "Restaurant",
+        rating: "4.8",
+      },
+      {
+        id: 3,
+        icons: <ion-icon name="wine-outline"></ion-icon>,
+        name: "Wine",
+        rating: "4.7",
+      },
+      {
+        id: 4,
+        icons: <ion-icon name="pizza-outline"></ion-icon>,
+        name: "Pizza",
+        rating: "4.6",
+      }
+    ],
+  };
+}
