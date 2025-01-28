d96288502effc483c511d66183e8b1d40a648ca2
ayuarista <aristaadewi8@gmail.com>
2025-01-28 13:49:13 +0800

    add script.js

--- src/script.js
+++ src/script.js
@@ -1,30 +1,10 @@
-function category() {
+function categoryItem() {
   return {
     items: [
-      {
-        id: 1,
-        icons: <ion-icon name="subway-outline"></ion-icon>,
-        name: "Beach",
-        rating: "4.9",
-      },
-      {
-        id: 2,
-        icons: <ion-icon name="restaurant-outline"></ion-icon>,
-        name: "Restaurant",
-        rating: "4.8",
-      },
-      {
-        id: 3,
-        icons: <ion-icon name="wine-outline"></ion-icon>,
-        name: "Wine",
-        rating: "4.7",
-      },
-      {
-        id: 4,
-        icons: <ion-icon name="pizza-outline"></ion-icon>,
-        name: "Pizza",
-        rating: "4.6",
-      }
-    ],
+        { id: 1, icon: "subway-outline", name: "Beach", rating: "4.9" },
+        { id: 2, icon: "restaurant-outline", name: "Restaurant", rating: "4.8" },
+        { id: 3, icon: "wine-outline", name: "Wine", rating: "4.7" },
+        { id: 4, icon: "pizza-outline", name: "Pizza", rating: "4.6" }
+      ]
   };
 }
