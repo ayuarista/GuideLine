a6063d56b1b08c74ecd8a9e831aca9e446fd3e14
ayuarista <aristaadewi8@gmail.com>
2025-01-30 11:05:48 +0800

    fix styling

--- src/script.js
+++ src/script.js
@@ -1,139 +1,184 @@
 function categoryItem() {
   return {
     items: [
-        { id: 1, icon: "subway-outline", name: "Beach", rating: "4.9" },
-        { id: 2, icon: "restaurant-outline", name: "Restaurant", rating: "4.8" },
-        { id: 3, icon: "wine-outline", name: "Wine", rating: "4.7" },
-        { id: 4, icon: "pizza-outline", name: "Pizza", rating: "4.6" }
-      ]
+      { id: 1, icon: "subway-outline", name: "Beach", rating: "4.9" },
+      { id: 2, icon: "restaurant-outline", name: "Restaurant", rating: "4.8" },
+      { id: 3, icon: "wine-outline", name: "Wine", rating: "4.7" },
+      { id: 4, icon: "pizza-outline", name: "Pizza", rating: "4.6" },
+    ],
   };
 }
-function popularPlace(){
-    return{
-        items:[
-            {
-                id: 1,
-                discount: "20%",
-                image: "https://plus.unsplash.com/premium_photo-1707944422423-314b12c3885a?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                title: "Indonesia",
-                location: "Bali, Nusa Penida",
-            },
-            {
-                id: 2,
-                discount: "20%",
-                image: "https://images.unsplash.com/photo-1584872589930-e99fe5bf4408?q=80&w=877&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                title: "China",
-                location: "China, Beijing",
-            },
-            {
-                id: 3,
-                discount: "20%",
-                image: "https://images.unsplash.com/photo-1542051841857-5f90071e7989?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                title: "Japan",
-                location: "Japan, Tokyo",
-            },
-            {
-                id: 4,
-                discount: "20%",
-                image: "https://images.unsplash.com/photo-1472148439583-1f4cf81b80e0?q=80&w=1031&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                title: "Singapore",
-                location: "Singapore, Paris",
-            },
-        ]
-    }
+function popularPlace() {
+  return {
+    items: [
+      {
+        id: 1,
+        discount: "20%",
+        image:
+          "https://plus.unsplash.com/premium_photo-1707944422423-314b12c3885a?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Indonesia",
+        location: "Bali, Nusa Penida",
+      },
+      {
+        id: 2,
+        discount: "20%",
+        image:
+          "https://images.unsplash.com/photo-1584872589930-e99fe5bf4408?q=80&w=877&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "China",
+        location: "China, Beijing",
+      },
+      {
+        id: 3,
+        discount: "20%",
+        image:
+          "https://images.unsplash.com/photo-1542051841857-5f90071e7989?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Japan",
+        location: "Japan, Tokyo",
+      },
+      {
+        id: 4,
+        discount: "20%",
+        image:
+          "https://images.unsplash.com/photo-1472148439583-1f4cf81b80e0?q=80&w=1031&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Singapore",
+        location: "Singapore, Paris",
+      },
+    ],
+  };
 }
 
-function trendingPlace(){
-    return{
-        items:[
+function trendingPlace() {
+  return {
+    items: [
+      {
+        id: 1,
+        image:
+          "https://images.unsplash.com/photo-1736185669686-f302d6274f23?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Beijing Tour",
+        desc: "gave an experience, never you felt before.",
+        price: "Rp. 5.500.000",
+        rating: "4.8 (329)",
+        star: 3,
+        duration: "8 days",
+        facilities: ["breakfast", "wifi"],
+      },
+      {
+        id: 2,
+        image:
+          "https://images.unsplash.com/photo-1529551739587-e242c564f727?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Dubai Tour",
+        desc: "gave an experience, never you felt before.",
+        price: "Rp. 10.500.000",
+        rating: "4.8 (329)",
+        star: 4,
+        duration: "8 days",
+        facilities: ["breakfast", "wifi"],
+      },
+      {
+        id: 3,
+        image:
+          "https://images.unsplash.com/photo-1592988008857-f73f31949462?q=80&w=1794&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "USA Tour",
+        desc: "gave an experience, never you felt before.",
+        price: "Rp. 5.500.000",
+        rating: "4.8 (329)",
+        star: 4,
+        duration: "8 days",
+        facilities: ["breakfast", "wifi"],
+      },
+      {
+        id: 4,
+        image:
+          "https://images.unsplash.com/photo-1700880325435-d13cbf5d698f?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Paris Tour",
+        desc: "gave an experience, never you felt before.",
+        price: "Rp. 15.500.000",
+        rating: "4.8 (10.872)",
+        star: 4,
+        duration: "8 days",
+        facilities: ["breakfast", "wifi"],
+      },
+      {
+        id: 5,
+        image:
+          "https://plus.unsplash.com/premium_photo-1715106090962-9def47b4d8bc?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Japan Tour",
+        desc: "gave an experience, never you felt before.",
+        price: "Rp. 5.500.000",
+        rating: "4.9 (20.735)",
+        star: 4,
+        duration: "4 days",
+        facilities: ["breakfast", "wifi"],
+      },
+      {
+        id: 6,
+        image:
+          "https://images.unsplash.com/photo-1718910690209-4901217cb277?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Beijing Tour",
+        desc: "gave an experience, never you felt before.",
+        price: "Rp. 5.500.000",
+        rating: "4.8 (329)",
+        star: 4,
+        duration: "8 days",
+        facilities: ["breakfast", "wifi"],
+      },
+      {
+        id: 7,
+        image:
+          "https://images.unsplash.com/photo-1647588830107-30cbdd140c48?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Beijing Tour",
+        desc: "gave an experience, never you felt before.",
+        price: "Rp. 5.500.000",
+        rating: "4.8 (329)",
+        star: 4,
+        duration: "8 days",
+        facilities: ["breakfast", "wifi"],
+      },
+      {
+        id: 8,
+        image:
+          "https://images.unsplash.com/photo-1647588830219-27f925040ea8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Beijing Tour",
+        desc: "gave an experience, never you felt before.",
+        price: "Rp. 5.500.000",
+        rating: "4.8 (329)",
+        star: 4,
+        duration: "8 days",
+        facilities: ["breakfast", "wifi"],
+      },
+    ],
+  };
+}
+
+function bestPlace() {
+    return {
+        items: [
             {
                 id: 1,
-                image: "https://images.unsplash.com/photo-1736185669686-f302d6274f23?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                title: "Beijing Tour",
-                desc: "gave an experience, never you felt before.",
-                price: "Rp. 5.500.000",
-                rating: "4.8 (329)",
-                star: 3,
-                duration: "8 days",
-                facilities: ["breakfast", "wifi"]
+                rating: "4.9",
+                images: [
+                    "https://images.unsplash.com/photo-1660405801491-7fe4cc1d3ccb?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    "https://images.unsplash.com/photo-1737270019710-62b36a249aca?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                ],
+                titles: ["Fokuw Lake", "Mystic Falls"],
+                categories: ["Lake", "Waterfall"],
+                mainImage: "https://images.unsplash.com/photo-1660404865237-72f821b40690?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                mainTitle: "Crystal Bay",
+                mainCategory: "Beach"
             },
             {
                 id: 2,
-                image: "https://images.unsplash.com/photo-1529551739587-e242c564f727?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                title: "Dubai Tour",
-                desc: "gave an experience, never you felt before.",
-                price: "Rp. 10.500.000",
-                rating: "4.8 (329)",
-                star: 4,
-                duration: "8 days",
-                facilities: ["breakfast", "wifi"]
-            },
-            {
-                id: 3,
-                image: "https://images.unsplash.com/photo-1592988008857-f73f31949462?q=80&w=1794&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                title: "USA Tour",
-                desc: "gave an experience, never you felt before.",
-                price: "Rp. 5.500.000",
-                rating: "4.8 (329)",
-                star: 4,
-                duration: "8 days",
-                facilities: ["breakfast", "wifi"]
-            },
-            {
-                id: 4,
-                image: "https://images.unsplash.com/photo-1700880325435-d13cbf5d698f?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                title: "Paris Tour",
-                desc: "gave an experience, never you felt before.",
-                price: "Rp. 15.500.000",
-                rating: "4.8 (10.872)",
-                star: 4,
-                duration: "8 days",
-                facilities: ["breakfast", "wifi"]
-            },
-            {
-                id: 5,
-                image: "https://plus.unsplash.com/premium_photo-1715106090962-9def47b4d8bc?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                title: "Japan Tour",
-                desc: "gave an experience, never you felt before.",
-                price: "Rp. 5.500.000",
-                rating: "4.9 (20.735)",
-                star: 4,
-                duration: "4 days",
-                facilities: ["breakfast", "wifi"]
-            },
-            {
-                id: 6,
-                image: "https://images.unsplash.com/photo-1718910690209-4901217cb277?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                title: "Beijing Tour",
-                desc: "gave an experience, never you felt before.",
-                price: "Rp. 5.500.000",
-                rating: "4.8 (329)",
-                star: 4,
-                duration: "8 days",
-                facilities: ["breakfast", "wifi"]
-            },
-            {
-                id: 7,
-                image: "https://images.unsplash.com/photo-1647588830107-30cbdd140c48?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                title: "Beijing Tour",
-                desc: "gave an experience, never you felt before.",
-                price: "Rp. 5.500.000",
-                rating: "4.8 (329)",
-                star: 4,
-                duration: "8 days",
-                facilities: ["breakfast", "wifi"]
-            },
-            {
-                id: 8,
-                image: "https://images.unsplash.com/photo-1647588830219-27f925040ea8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                title: "Beijing Tour",
-                desc: "gave an experience, never you felt before.",
-                price: "Rp. 5.500.000",
-                rating: "4.8 (329)",
-                star: 4,
-                duration: "8 days",
-                facilities: ["breakfast", "wifi"]
+                rating: "4.9",
+                images: [
+                    "https://images.unsplash.com/photo-1647588830107-30cbdd140c48?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    "https://images.unsplash.com/photo-1529551739587-e242c564f727?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                ],
+                titles: ["Fokuw Lake", "Mystic Falls"],
+                categories: ["Lake", "Waterfall"],
+                mainImage: "https://images.unsplash.com/photo-1689775885271-ce8f884886d6?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                mainTitle: "Crystal Bay",
+                mainCategory: "Beach"
             },
         ]
-    }
+    };
 }
